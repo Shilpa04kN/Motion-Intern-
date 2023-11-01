39424de27d161cea5e9c54fb5f27897095da7b01
Shilpa04kN <125974207+Shilpa04kN@users.noreply.github.com>
2023-11-01 16:58:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// JavaScript to toggle the visibility of the search box
+function toggleSearchBox() {
+  var searchBox = document.getElementById("search-box");
+  if (searchBox.style.display === "block") {
+    searchBox.style.display = "none";
+  } else {
+    searchBox.style.display = "block";
+  }
+}
+
+
+
+
